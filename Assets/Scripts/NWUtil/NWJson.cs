42d4fabe19c1f8a87e656b0ecade4289800bb0f4
LeeKeunHo <unionlkh@naver.com>
2018-08-07 00:30:53 +0900

    json 유틸리티를 dll에서 유니티 패키지로 임포트하는 방식으로 변경

--- Assets/Scripts/NWUtil/NWJson.cs
+++ Assets/Scripts/NWUtil/NWJson.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class NWJson {
     public static string ToJson(object obj) {
-        return JsonWriter.Serialize(obj);
+        JsonWriter writer = new JsonWriter();
+        return writer.Write(obj);
     }
     public static Dictionary<K, T> FromJsonToDictionary<K, T>(string json) {
-        return JsonReader.Deserialize<Dictionary<K, T>>(json);
+        JsonReader reader = new JsonReader();
+        return reader.Read<Dictionary<K, T>>(json);
     }
     public static List<T> FromJsonToList<T>(string json) {
-        return JsonReader.Deserialize<List<T>>(json);
+        JsonReader reader = new JsonReader();
+        return reader.Read<List<T>>(json);
     }
 }
