919b99330521e0fd72359d4f3dcc55de08c9dcef
LeeKeunHo <unionlkh@naver.com>
2018-08-12 00:16:48 +0900

    UserImportAssetInfo로 래핑 완료

--- Assets/Scripts/DataManager/NW_UserImportAssetManager.cs
+++ Assets/Scripts/DataManager/NW_UserImportAssetManager.cs
@@ -11,60 +11,43 @@ using UnityEngine.UI;//only test
 /// 여기 있는 파일은 아틀라스를 그대로 한장의 그림으로 익스포트 한 것으로, 그림 곳곳의 요소들을 수정하여 해당 오브젝트에 실제로 적용할 수 있습니다.
 /// </summary>
 public class NW_UserImportAssetManager : MonoBehaviour {
-
-    private class UserImportAssetInfo {
-        public object asset;
-        public string assetName;
-        public UserImportAssetType type;
-        public UserImportAssetInfo(string name, Texture2D texture) {
-            this.assetName = name;
-            this.asset = texture;
-            this.type = UserImportAssetType.Texture2D;
-        }
-        public UserImportAssetInfo(string name, AudioClip audioClip) {
-            this.assetName = name;
-            this.asset = audioClip;
-            this.type = UserImportAssetType.AudioClip;
-        }
-    }
-
     /// <summary>
     /// 유저 에셋 폴더의 모든 하위폴더에 존재하는 모든 텍스쳐를 usage별로 나열합니다. 각 usage의 리스트에 2개 이상의 텍스쳐가 들어있는 경우, 해당 텍스쳐의 Usage가 충돌한 것입니다.
     /// </summary>
-    public Dictionary<string, List<Texture2D>> tempTextureDic = new Dictionary<string, List<Texture2D>>();// [usage / texture]
+    public Dictionary<string, List<UserImportAssetInfo>> tempTextureDic = new Dictionary<string, List<UserImportAssetInfo>>();// [usage / texture]
     /// <summary>
     /// 유저 에셋 폴더의 모든 하위폴더에 존재하는 모든 오디오 클립을 usage별로 나열합니다. 각 usage의 리스트에 2개 이상의 오디오 클립이 들어있는 경우, 해당 오디오 클립의 Usage가 충돌한 것입니다.
     /// </summary>
-    public Dictionary<string, List<AudioClip>> tempAudioClipDic = new Dictionary<string, List<AudioClip>>();// [usage / audioSource]
+    public Dictionary<string, List<UserImportAssetInfo>> tempAudioClipDic = new Dictionary<string, List<UserImportAssetInfo>>();// [usage / audioSource]
 
     /// <summary>
     /// 유저 에셋 폴더에서 임포트된 모든 usage별 텍스쳐
     /// </summary>
-    public static Dictionary<string, Texture2D> UserImportTextureDic = new Dictionary<string, Texture2D>();
-    public static Dictionary<string, AudioClip> UserImportAudioClipDic = new Dictionary<string, AudioClip>();
+    public static Dictionary<string, UserImportAssetInfo> UserImportTextureDic = new Dictionary<string, UserImportAssetInfo>();
+    public static Dictionary<string, UserImportAssetInfo> UserImportAudioClipDic = new Dictionary<string, UserImportAssetInfo>();
 
     private void Awake() {
         this.Initialize_UserImportAssets();
 
         //인게임 오브젝트 테스트 코드
-        GameObject.Find("Cube1").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage1"];
-        GameObject.Find("Cube2").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage2"];
-        GameObject.Find("Cube3").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage1"];
+        GameObject.Find("Cube1").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage1"].texture;
+        GameObject.Find("Cube2").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage2"].texture;
+        GameObject.Find("Cube3").GetComponent<MeshRenderer>().material.mainTexture = UserImportTextureDic["usage1"].texture;
 
         //UI 오브젝트 테스트 코드
         //GameObject.Find("Image (1)").GetComponent<Image>().material.mainTexture = this.finalUserImportTextureDic["usage1"];//이렇게 하면 디폴트 매터리얼이 적용되어, 전체 UI에 적용됨
-        Rect rec = new Rect(0, 0, UserImportTextureDic["usage1"].width, UserImportTextureDic["usage1"].height);
-        GameObject.Find("Image (1)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"], rec, new Vector2(0, 0), 1);
-        GameObject.Find("Image (2)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"], rec, new Vector2(0, 0), 1);
-        GameObject.Find("Image (3)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"], rec, new Vector2(0, 0), 1);
-        GameObject.Find("Image (4)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"], rec, new Vector2(0, 0), 1);
+        Rect rec = new Rect(0, 0, UserImportTextureDic["usage1"].texture.width, UserImportTextureDic["usage1"].texture.height);
+        GameObject.Find("Image (1)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"].texture, rec, new Vector2(0, 0), 1);
+        GameObject.Find("Image (2)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"].texture, rec, new Vector2(0, 0), 1);
+        GameObject.Find("Image (3)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"].texture, rec, new Vector2(0, 0), 1);
+        GameObject.Find("Image (4)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage1"].texture, rec, new Vector2(0, 0), 1);
 
-        Rect rec2 = new Rect(0, 0, UserImportTextureDic["usage2"].width, UserImportTextureDic["usage2"].height);
-        GameObject.Find("Image (5)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"], rec2, new Vector2(0, 0), 1);
-        GameObject.Find("Image (6)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"], rec2, new Vector2(0, 0), 1);
-        GameObject.Find("Image (7)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"], rec2, new Vector2(0, 0), 1);
-        GameObject.Find("Image (8)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"], rec2, new Vector2(0, 0), 1);
-        GameObject.Find("Image (9)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"], rec2, new Vector2(0, 0), 1);
+        Rect rec2 = new Rect(0, 0, UserImportTextureDic["usage2"].texture.width, UserImportTextureDic["usage2"].texture.height);
+        GameObject.Find("Image (5)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"].texture, rec2, new Vector2(0, 0), 1);
+        GameObject.Find("Image (6)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"].texture, rec2, new Vector2(0, 0), 1);
+        GameObject.Find("Image (7)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"].texture, rec2, new Vector2(0, 0), 1);
+        GameObject.Find("Image (8)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"].texture, rec2, new Vector2(0, 0), 1);
+        GameObject.Find("Image (9)").GetComponent<Image>().sprite = Sprite.Create(UserImportTextureDic["usage2"].texture, rec2, new Vector2(0, 0), 1);
     }
 
     public void Initialize_UserImportAssets() {
@@ -82,13 +65,13 @@ public class NW_UserImportAssetManager : MonoBehaviour {
         Debug.Log("User Import Asset Conflict : " + isConflict);
 
         //step 4. 생성한 딕셔너리를 이용해, 실제 에셋들을 로드한다. 0번 에셋만 로드한다. (유저가 다른 에셋을 임포트하고자 원하면, 그것을 리스트의 0번에 놓는다.)텍스쳐의 경우 아틀라스로 묶는다.
-        foreach (KeyValuePair<string, List<Texture2D>> kv in this.tempTextureDic){
+        foreach (KeyValuePair<string, List<UserImportAssetInfo>> kv in this.tempTextureDic){
             UserImportTextureDic[kv.Key] = kv.Value[0];
-            Debug.Log("texture added in FinalTextureList. key : " + kv.Key + ", name : " + kv.Value[0].name);
+            Debug.Log("texture added in FinalTextureList. key : " + kv.Key + ", name : " + kv.Value[0].texture.name);
         }
-        foreach(KeyValuePair<string, List<AudioClip>> kv in this.tempAudioClipDic) {
+        foreach(KeyValuePair<string, List<UserImportAssetInfo>> kv in this.tempAudioClipDic) {
             UserImportAudioClipDic[kv.Key] = kv.Value[0];
-            Debug.Log("audioclip added in FinalAudioClipList. key : " + kv.Key + ", name : " + kv.Value[0].name);
+            Debug.Log("audioclip added in FinalAudioClipList. key : " + kv.Key + ", name : " + kv.Value[0].texture.name);
         }
 
         //step 5. 위에서 생성한 배열들에 필요한 잔여 작업을 수행한다.
@@ -109,9 +92,9 @@ public class NW_UserImportAssetManager : MonoBehaviour {
             tempDic = manifestDic["Texture"];
             foreach (KeyValuePair<string, string> kv in tempDic) {
                 if (!this.tempTextureDic.ContainsKey(kv.Key)) {
-                    this.tempTextureDic.Add(kv.Key, new List<Texture2D>());
+                    this.tempTextureDic.Add(kv.Key, new List<UserImportAssetInfo>());
                 }
-                this.tempTextureDic[kv.Key].Add(WWWLoadUserImportAsset(kv.Key, Path.Combine(assetPath, kv.Value), UserImportAssetType.Texture2D) as Texture2D);
+                this.tempTextureDic[kv.Key].Add(new UserImportAssetInfo(manifestFileName, WWWLoadUserImportAsset(kv.Key, Path.Combine(assetPath, kv.Value), UserImportAssetType.Texture2D) as Texture2D));
             }
         } catch {
             Debug.LogWarning("There is no Texture in this asset : " + manifestFileName);
@@ -122,28 +105,28 @@ public class NW_UserImportAssetManager : MonoBehaviour {
             tempDic = manifestDic["AudioClip"];
             foreach (KeyValuePair<string, string> kv in tempDic) {
                 if (!this.tempAudioClipDic.ContainsKey(kv.Key)) {
-                    this.tempAudioClipDic.Add(kv.Key, new List<AudioClip>());
+                    this.tempAudioClipDic.Add(kv.Key, new List<UserImportAssetInfo>());
                 }
-                this.tempAudioClipDic[kv.Key].Add(WWWLoadUserImportAsset(kv.Key, Path.Combine(assetPath, kv.Value), UserImportAssetType.AudioClip) as AudioClip);
+                this.tempAudioClipDic[kv.Key].Add(new UserImportAssetInfo(manifestFileName, WWWLoadUserImportAsset(kv.Key, Path.Combine(assetPath, kv.Value), UserImportAssetType.AudioClip) as AudioClip));
             }
         } catch {
             Debug.LogWarning("There is no AudioClip in this asset : " + manifestFileName);
         }
     }
     private bool DetermineConflict() {
-        foreach(KeyValuePair<string, List<Texture2D>> kv in this.tempTextureDic) {
+        foreach(KeyValuePair<string, List<UserImportAssetInfo>> kv in this.tempTextureDic) {
             if(kv.Value.Count > 1) {
                 return true;
             }
         }
-        foreach(KeyValuePair<string, List<AudioClip>> kv in this.tempAudioClipDic) {
+        foreach(KeyValuePair<string, List<UserImportAssetInfo>> kv in this.tempAudioClipDic) {
             if(kv.Value.Count > 1) {
                 return true;
             }
         }
         return false;
     }
-    private List<Texture2D> MakeAtlasFromTextureList(Dictionary<string, Texture2D> dic) {
+    private List<Texture2D> MakeAtlasFromTextureList(Dictionary<string, UserImportAssetInfo> dic) {
         //이제 캐릭터 일러스트, 아틀라스 크기가 모두 확정되었으므로, 이 함수 작성 가능 (텍스쳐의 사용처가 전부 캐릭터 일러이므로, 그냥 막 묶으면 됨)
         //언제 어떤 캐릭이 쓰일지 예측할 방법은 없음.
         //List<Texture2D> rst = new List<Texture2D>();
@@ -203,3 +186,28 @@ public static partial class CONST_VALUES {
     public const int CHARECTOR_ILUSE_ATLAS_MAX_SIZE = 2048;
 }
 
+public class UserImportAssetInfo {
+    public object asset;
+    public string assetName;
+    public UserImportAssetType type;
+    public UserImportAssetInfo(string name, Texture2D texture) {
+        this.assetName = name;
+        this.asset = texture;
+        this.type = UserImportAssetType.Texture2D;
+    }
+    public UserImportAssetInfo(string name, AudioClip audioClip) {
+        this.assetName = name;
+        this.asset = audioClip;
+        this.type = UserImportAssetType.AudioClip;
+    }
+    public Texture2D texture {
+        get {
+            return asset as Texture2D;
+        }
+    }
+    public AudioClip audioClip {
+        get {
+            return asset as AudioClip;
+        }
+    }
+}
